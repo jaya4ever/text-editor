36805447f62a1389f0ac1cc3e660f11d6b599feb
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2023-02-20 20:47:30 -0600

    trying to find out issues in my file

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,7 +4,6 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
-const { request } = require('express');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -28,13 +27,4 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute(({request}) => ['style' , 'script' , 'worker']).includes(request.destination),
-new StaleWhileRevalidate({
-  cacheName:'asset-cache',
-  plugins:[
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-  ],
-});
- 
+registerRoute();
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,26 +13,9 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
-  console.error('putDb not implemented');
-  const jate_db = await openDB('jate_db', 1);
-  const tx = jate_db.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, jate: content });
-  const result = await request;
-  console.log('data saved to the database', result);
+export const putDb = async (content) => console.error('putDb not implemented');
 
-};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {console.error('getDb not implemented');
-const jate_db = await openDB('jate_db', 1);
-  const tx = jate_db.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.get(1);
-  const result = await request;
-  console.log('result.jate', result);
-  // If result is defined, return it. Otherwise, return undefined (instead of breaking). Optional chaining.
-  return result?.jate;
-};
+export const getDb = async () => console.error('getDb not implemented');
 
 initdb();
