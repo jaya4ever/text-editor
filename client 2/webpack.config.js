fd31f713e700a3dd1a304cb2ab6e83a33217e16b
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2023-02-21 11:46:32 -0600

    did the modification in webpack

--- client 2/webpack.config.js
+++ client 2/webpack.config.js
@@ -4,55 +4,43 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
 
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js',
-      //database: './src/js/database.js'
+      install: './src/js/install.js'
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    // TODO: Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
-      // Adding Webpack plugin to generate HTML and inject our bundles
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'JATE'
+        title: 'Text Editor',
       }),
-      // TODO: Add and configure workbox plugins for a service worker and manifest file.
-      // service worker
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-      // manifest.json
       new WebpackPwaManifest({
+        name: 'Text Editor',
+        publicPath: '/',
+        start_url: '/',
         fingerprints: false,
-        inject: true,
-        name: 'text-editor',
-        short_name: 'JATE',
-        description: 'Just Another Text Editor!',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
-        icons: [
-          {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
-          },
-        ],
-      }),
+        icons: [{
+          src:  path.resolve('./src/images/logo.png'),
+          destination: path.join('assets', 'icons'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        },]
+      })
     ],
-
+// TODO: Add CSS loaders and babel to webpack.
     module: {
-      // TODO: Add CSS loaders and babel to webpack.
       rules: [
         {
           test: /\.css$/i,
@@ -61,14 +49,15 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-          use: {
+          use:{
             loader: 'babel-loader',
-            options: {
+            options:{
               presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },
+        
       ],
     },
   };
